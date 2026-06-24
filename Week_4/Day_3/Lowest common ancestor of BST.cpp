dcbe9cc6998126430ecc2ee81346252644615f69
yjayant01 <yjayant987@gmail.com>
2026-06-24 21:40:42 +0530

    Till Week_4 Day_3

---
+++ Week_4/Day_3/Lowest common ancestor of BST.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        while (root) {
+            
+            if (p->val < root->val && q->val < root->val)
+                root = root->left;
+
+            else if (p->val > root->val && q->val > root->val)
+                root = root->right;
+
+            else
+                return root;
+        }
+
+        return NULL;
+    }
+};
